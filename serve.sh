4507d4c8985a7134edc80d91e2e7c7604c3c9958
Rick Cogley <rick.cogley@esolia.co.jp>
2021-10-11 13:27:32 +0900

    Add --ignoreCache for CI build time test

--- serve.sh
+++ serve.sh
@@ -3,4 +3,4 @@
 echo "Setup PROdb API Token in Env..."
 export PRODBTOKEN15331=$(cat $HOME/.ssh/tokens/prodb-01)
 echo "Serve hugo (ctrl-c to exit)..."
-hugo server --navigateToChanged --buildDrafts --buildFuture --watch --verbose --port=1317 --enableGitInfo 
+hugo server --navigateToChanged --buildDrafts --buildFuture --watch --verbose --port=1317 --enableGitInfo --ignoreCache
